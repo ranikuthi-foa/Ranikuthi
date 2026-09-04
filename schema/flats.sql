5e6217778a3b2138e6193ef0782e6678a1d11846
ranikuthi-foa <ranikuthi.foa@gmail.com>
2026-09-05 00:53:33 +0530

    Create flats table with relevant fields

--- schema/flats.sql
+++ schema/flats.sql
@@ -1,1 +1,8 @@
-
+-- Flats table
+CREATE TABLE flats (
+    flat_id SERIAL PRIMARY KEY,
+    owner_id INT REFERENCES users(user_id),
+    flat_number VARCHAR(20) UNIQUE NOT NULL,
+    sq_ft INT,
+    status VARCHAR(20) DEFAULT 'occupied'
+);
