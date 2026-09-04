68ab5fb61684fd9c3c946ff00d17ccb7e860af14
ranikuthi-foa <ranikuthi.foa@gmail.com>
2026-09-05 00:54:27 +0530

    Create payments table in payments.sql

--- schema/payments.sql
+++ schema/payments.sql
@@ -1,1 +1,13 @@
-
+-- Payments table
+CREATE TABLE payments (
+    payment_id SERIAL PRIMARY KEY,
+    auto_voucher_no VARCHAR(50) UNIQUE NOT NULL,
+    manual_voucher_no VARCHAR(50) NOT NULL,
+    ref_invoice_no VARCHAR(50),
+    invoice_date DATE,
+    vendor_id INT,
+    vendor_name VARCHAR(100),
+    amount NUMERIC(12,2) NOT NULL,
+    payment_mode VARCHAR(50),
+    narration TEXT
+);
